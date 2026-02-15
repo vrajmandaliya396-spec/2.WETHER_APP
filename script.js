f88f71b30c445167c9cc115b1861ba6b33f29e5c
vrajmandaliya396-spec <vrajmandaliya396@gmail.com>
2026-02-15 22:43:53 +0530

    add some functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+var search_btn = document.querySelector(".search-btn");
+var data_container = document.querySelector(".data_container");
+var city_input = document.querySelector("city-input");
+var temp_container = document.querySelector(".temp-container");
+
+data_container.style.display = "none";
+
+search_btn.addEventListener("click", function(e){
+    
+            data_container.style.display = "";
+});
+
